e0aa8cec1c95fc211afcafa50e6e144f18696e5a
azartechnet <azartechnet@gmail.com>
2025-03-24 19:02:59 +0530

    updated

--- ReactJs/mrgproj/src/index.js
+++ ReactJs/mrgproj/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-function Sample()
+/*function Sample()
 {
   return(
     <div>
@@ -12,4 +12,50 @@ function Sample()
   )
 }
 const r1=ReactDOM.createRoot(document.getElementById('root'))
-r1.render(<Sample/>)
+r1.render(<Sample/>)*/
+
+//Dynamic Rendering
+/*const App=()=>{
+  return(
+    <div>
+      <h1>Dynamic Rendering</h1>
+      <button onClick={()=>alert("Button Clicked")}>Click Me</button>
+      <p>Todays date::{new Date().toLocaleDateString()}</p>
+    </div>
+  )
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<App/>)*/
+
+//React without JSX
+// const App=<h1>Welcome to the Application</h1>
+// const r1=ReactDOM.createRoot(document.getElementById('root'))
+// r1.render(App)
+//React with JSX
+/*const App=()=>{
+  return React.createElement("h1",null,"Welcome to the Application")
+  }
+  const r1=ReactDOM.createRoot(document.getElementById('root'))
+  r1.render(React.createElement(App,null))*/
+
+  //React list
+
+  const MyElem1=()=>{
+    return(
+      <div>
+        <h1 style={{backgroundColor:"red"}}>My First Element</h1>
+        <ul>
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+        </ul>
+        <ol>
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+        </ol>
+      </div>
+    )
+  }
+  const r1=ReactDOM.createRoot(document.getElementById('root'))
+  r1.render(<MyElem1/>)

e061fff81795a19ff90be40b63b81a13dc1e2077
azartechnet <azartechnet@gmail.com>
2025-06-30 10:59:35 +0530

    Update nodep1.js

--- Nodejs/nodep1.js
+++ Nodejs/nodep1.js
@@ -1,20 +1,20 @@
-// const h1=require('http')
-// const server=h1.createServer((req,res)=>{
-//     res.writeHead(200,{'Content-Type':'text/plain'});
-//     res.end('Welcome to the Application')
-// }).listen(8081)
-// console.log("Server is Running..")
+const h1=require('http')
+const server=h1.createServer((req,res)=>{
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.end('Welcome to the Application')
+}).listen(8081)
+console.log("Server is Running..")
 
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('Welcome to the Application\n');
-    res.write('This is a multi-line response.\n');
-    res.write('Enjoy your stay!\n');
-    res.end(); // End the response
-});
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, { 'Content-Type': 'text/plain' });
+//     res.write('Welcome to the Application\n');
+//     res.write('This is a multi-line response.\n');
+//     res.write('Enjoy your stay!\n');
+//     res.end(); // End the response
+// });
 
-server.listen(8081, () => {
-    console.log("Server is Running on port 8081...");
-});
+// server.listen(8081, () => {
+//     console.log("Server is Running on port 8081...");
+// });
